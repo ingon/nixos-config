7938b8aa24240612f963e9dc0a3f1fbe4b41b88f
Nikolay Petrov <sungon@gmail.com>
2024-10-23 21:30:01 -0400

    neovim: tabs focus on file

--- home/neovim.nix
+++ home/neovim.nix
@@ -29,6 +29,7 @@
         config = ''
           nmap <silent> <leader>t :NERDTreeTabsToggle<CR>
           let g:nerdtree_tabs_open_on_console_startup=1
+          let g:nerdtree_tabs_smart_startup_focus=2
         '';
       }
       vim-surround
