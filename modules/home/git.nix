5e52495dee41dfc1c2f2f390adca1892700a076d
Nikolay Petrov <sungon@gmail.com>
2024-10-29 12:34:01 -0400

    migrate git to full module

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,13 +1,32 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.ingon.home-modules.git;
+in
 {
-  programs.git = {
-    enable = true;
-    userEmail = "sungon@gmail.com";
-    userName = "Nikolay Petrov";
-    diff-so-fancy.enable = true;
-    extraConfig = {
-      rerere.enabled = true;
-      init.defaultBranch = "main";
+  options.ingon.home-modules.git = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "home-manager git config";
+    };
+
+    email = lib.mkOption {
+      type = lib.types.str;
+      default = "sungon@gmail.com";
+      description = "git config user.email";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userEmail = cfg.email;
+      userName = "Nikolay Petrov";
+      diff-so-fancy.enable = true;
+      extraConfig = {
+        rerere.enabled = true;
+        init.defaultBranch = "main";
+      };
     };
   };
 }
