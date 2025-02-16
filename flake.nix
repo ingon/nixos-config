96af0329f0c65b283fa7244f82f84260c26a5e4a
Nikolay Petrov <sungon@gmail.com>
2025-02-16 12:58:46 -0500

    add hmac

--- flake.nix
+++ flake.nix
@@ -57,6 +57,22 @@
             connet.nixosModules.default
           ];
         };
+        hmac = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ({ ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+            ./hosts/hmac/configuration.nix
+            # home-manager.nixosModules.home-manager
+            # {
+            #   home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            # home-manager.users.niki = import ./hosts/fw/home-niki.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+            # }
+          ];
+        };
       };
     };
 }
